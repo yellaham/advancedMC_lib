2c4680d6ff1b115e6779b82507b5a2a979054ff2
Yousef El-Laham <yousef.ellaham@stonybrook.edu>
2019-08-04 10:50:04 -0700

    basic implementation of pmc .py

--- python/ais_lib.py
+++ python/ais_lib.py
@@ -1,6 +1,8 @@
 import numpy as np
+import scipy.stats as sp
+from scipy.stats import multivariate_normal as mvn
 
-def pmc(log_target, d, D=500, N=1, I=400, bounds=[-10,10]):
+def pmc(log_target, d, D=500, N=1, I=200, bounds=(-10,10)):
     """
     Runs the population Monte Carlo algorithm
     :param log_target: Logarithm of the target distribution
@@ -11,4 +13,40 @@ def pmc(log_target, d, D=500, N=1, I=400, bounds=[-10,10]):
     :param bounds: Prior to generate location parameters over [bounds]**d hypercube
     :return particles, weights, and estimate of normalizing constant
     """
+    # Initialize the mean of each proposal distribution
+    parents = np.random.uniform(bounds[0], bounds[1], (D, d))
+    mu = np.repeat(parents, N, axis=0)
 
+    # Initialize the covariance matrix
+    sig = np.eye(d)
+
+    # Initialize storage of particles and log weights
+    particles = np.zeros((D*N*(I+1), d))
+    log_weights = np.zeros(D*N*(I+1))
+
+    # Initialize start counter
+    start=0
+
+    # Loop for the algorithm
+    for i in range(I):
+        # Update start counter
+        stop = start + D*N
+        # Generate children particles
+        children = mu + (np.matmul(sig, np.random.randn(D*N, d).T)).T
+        particles[start:stop, :] = children
+        # Compute log proposal
+        log_prop = np.array([mvn.logpdf(children[m, :], mean=mu[m, :], cov=sig) for m in range(stop-start)])
+        # Compute log weights and store
+        log_w = log_target(children) - log_prop
+        log_weights[start:stop] = log_w
+        # Convert log weights to standard weights using LSE and normalize
+        w = np.exp(log_w - np.max(log_w))
+        w = w/np.sum(w)
+        # Resampling to obtain new parents
+        idx = np.random.choice(D*N, D, replace=True, p=w)
+        parents = children[idx, :]
+        mu = np.repeat(parents, N, axis=0)
+        # Update the start index
+        start = stop + 1
+
+    return particles, log_weights
--- python/test.py
+++ python/test.py
@@ -1,3 +1,18 @@
 import numpy as np
+import scipy.stats as sp
 import ais_lib
 
+# Define target distribution
+dim = 2
+gauss_target = lambda x: sp.multivariate_normal.logpdf(x, mean=np.zeros(dim), cov=np.eye(dim))
+
+# Run PMC
+theta, log_w = ais_lib.pmc(gauss_target, dim)
+
+# Convert to standard weights using LSE and normalize
+w = np.exp(log_w-np.max(log_w))
+w = w/np.sum(w)
+
+# Determine mean of the target distribution
+mu_est = np.average(theta, axis=0, weights=w)
+print(mu_est)
