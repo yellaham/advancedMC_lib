10a1ddfca3b3af1cab0ee74be09ad7dc620faf14
Yousef <yousef.ellaham@stonybrook.edu>
2019-08-25 17:23:30 -0400

    changed to 10d Gaussian target

--- python/estimate_mvn_gauss_test.py
+++ python/estimate_mvn_gauss_test.py
@@ -1,14 +1,16 @@
 import numpy as np
 import scipy.stats as sp
 import matplotlib.pyplot as plt
-import ais_lib
+import ais_lib as ais
 
 # Define target distribution
-dim = 2
-gauss_target = lambda x: sp.multivariate_normal.logpdf(x, mean=np.zeros(dim), cov=np.eye(dim))
+dim = 10
+mu_pi = np.ones(dim)
+sigma_pi = np.eye(dim)
+gauss_target = lambda x: sp.multivariate_normal.logpdf(x, mean=mu_pi, cov=sigma_pi)
 
 # Run PMC
-result_pmc = ais_lib.pmc(gauss_target, dim)
+result_pmc = ais.pmc(gauss_target, dim, weighting_scheme='DM')
 theta = result_pmc.particles
 log_w = result_pmc.log_weights
 
@@ -26,4 +28,8 @@ plt.show()
 
 # D etermine mean of the target distribution
 mu_est = np.average(theta, axis=0, weights=w)
-print(mu_est)
+print('Estimated mean: ', mu_est)
+
+# Compute the MSE
+MSE = (np.linalg.norm(mu_est-mu_pi)**2)/dim
+print('MSE: ',MSE)
